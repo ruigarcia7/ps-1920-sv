454d0d39a532a3c334614621a12c0d01a0856ae5
Rui <ruigarcia7@hotmail.com>
2020-04-29 19:28:02 +0100

    Added almost all front-end object classes.

---
+++ frontend2/Rugby/src/app/classes/staff.ts
@@ -0,0 +1,15 @@
+export class Staff {
+  constructor(
+  private id?: number,
+  private height?: string,
+  private weight?: string,
+  private athleteNumber?: string,
+  private comment?: string
+  ) {
+    this.id = id ? id : 0;
+    this.height = height ? height : '';
+    this.weight = weight ? weight : '';
+    this.athleteNumber = athleteNumber ? athleteNumber : '';
+    this.comment = comment ? comment : '';
+  }
+}
